ed5ad5cccd1ebaf1ae76f8cb467bcffc52635522
nyzs <oknakoca@gmail.com>
2025-02-19 13:24:57 +0100

    feat: setup drizzle with neon

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "drizzle-kit";
-import * as dotenv from "dotenv";
+import "dotenv/config";
 
-dotenv.config();
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+}
 
 export default defineConfig({
     out: "./drizzle",
--- packages/database/src/migrate.ts
+++ packages/database/src/migrate.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { migrate } from "drizzle-orm/neon-http/migrator";
+import "dotenv/config";
 
 if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not defined");
