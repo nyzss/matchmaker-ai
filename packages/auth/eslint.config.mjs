d91c71357cc9f55f70d1736088c81b29be86881c
nyzs <oknakoca@gmail.com>
2025-02-21 11:39:32 +0100

    feat: linting is now consistent throughout the app

--- packages/auth/eslint.config.mjs
+++ packages/auth/eslint.config.mjs
@@ -3,13 +3,9 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import { config as baseConfig } from "@repo/eslint-config/base";
+
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommended,
-    baseConfig,
-    [
-        {
-            ignores: ["node_modules", ".wrangler"],
-        },
-    ]
+    baseConfig
 );
